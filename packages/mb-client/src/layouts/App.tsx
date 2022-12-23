c3d36bef35c75bc4054f270d249feda0b5078595
Maksim Tsipun <mtsipun@wiley.com>
2022-12-23 17:35:46 +0300

    Add packages + nx

--- packages/mb-client/src/layouts/App.tsx
+++ packages/mb-client/src/layouts/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import 'normalize.css'
 import './App.scss'
+import { TestModel } from 'mb-shared';
 
 const App = () => {
+  const extrude = (): TestModel => {
+    return {
+      id: "12313",
+      createdAt: new Date(),
+    };
+  } 
+  
+  useEffect(() => {
+    console.log('🎁', extrude())
+  }, [])
+  
   return (
     <div className="app-wrapper">
       <div className={'app-header'}>🥎 Mini Bank</div>
---
+++ packages/mb-shared/index.ts
@@ -0,0 +1,4 @@
+export type TestModel = {
+  id: string;
+  createdAt: Date;
+}
